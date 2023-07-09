282721bba7ed45efda186976ceeb0c2084278a7a
elproducer <elproducer20@gmail.com>
2023-07-09 13:50:28 -0700

    fix: remove unused imports

--- src/app/components/Background/index.test.tsx
+++ src/app/components/Background/index.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Background, {BackgroundContext} from '.';
-import StartButton from '../StartButton';
 
 test('Background snapshot', () => {
   const {container} = render(
